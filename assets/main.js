4b72c7432da2141f0d811e39b06324549ec5294c
aidensymes <hello@aidensymes.com>
2018-12-07 11:10:52 -0500

    working scroll loop

--- assets/main.js
+++ assets/main.js
@@ -1,52 +1,63 @@
 // Main Js for site
 
 ////////////////////////////////////////////////////////////////////////////////
-var visible = false;
-
 $( window ).on('load', resizeMasthead);
 $( window ).on('resize', resizeMasthead);
-$( window ).on('load', toggleVisible);
+
+var offset;
+var docHeight;
 
 function resizeMasthead() {
   var height = $( window ).height()
   $('.masthead').height(height - 180);
-  $('.masthead__img').height(height - 90);
+  $('.masthead__img').height(height - 120);
+
+  var width = $('.masthead').width() - $('.masthead__img').width();
+  $('.masthead__img').css('left', width/2)
+
+  offset = $('.masthead').height();
+  docHeight = $('.wrapper').height()
 }
 
-function toggleVisible() {
+function toggleVisible(visible) {
   if (visible) {
-    $('#masthead__img--branded').css('display', 'block');
-    $('#masthead__img--covered').css('display', 'none');
-    $('#masthead__details').css('display', 'block');
+    $('.masthead__img--branded').css('display', 'block');
+    $('.masthead__img--covered').css('display', 'none');
+    $('.masthead__details').css('display', 'block');
   } else {
-    $('#masthead__img--branded').css('display', 'none');
-    $('#masthead__img--covered').css('display', 'block');
-    $('#masthead__details').css('display', 'none');
+    $('.masthead__img--branded').css('display', 'none');
+    $('.masthead__img--covered').css('display', 'block');
+    $('.masthead__details').css('display', 'none');
   }
 }
 
+function elementInView(elem){
+  return $(window).scrollTop() < $(elem).offset().top + $(elem).height() ;
+}
+
 $('document').ready(function() {
-    var origDocHeight = document.body.offsetHeight;
     var clone=$("body").contents().clone();
-    var offset = 300;
     clone.appendTo("body");
-    clone.prependTo("body");
+
+    var inView = true;
+    var counter = 0;
+    toggleVisible(false);
 
     $(document).scroll(function(){
-    var scrollWindowPos = $(document).scrollTop();
-    if(scrollWindowPos >= origDocHeight) {
-      // temporarily disabled
-      // $(document).scrollTop(0);
-    }
-    if(scrollWindowPos <= 0 ) {
-      // temporarily disabled
-      // $(document).scrollTop(origDocHeight);
-    }
-    if(scrollWindowPos >= origDocHeight - offset) {
-      toggleVisible()
-     }
-    if(scrollWindowPos <= 0 ) {
-      toggleVisible()
-    }
+      var scrollWindowPos = $(document).scrollTop();
+      if(scrollWindowPos >= docHeight + 90) {
+        $(document).scrollTop(0);
+        counter += 1;
+      }
+      if(elementInView($('.masthead').first())) {
+        inView = true;
+      } else {
+        inView = false;
+        if (counter%2 == 0) {
+          toggleVisible(true)
+        } else {
+          toggleVisible(false)
+        }
+      }
     });
 });
